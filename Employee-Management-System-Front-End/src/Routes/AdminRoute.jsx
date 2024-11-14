57b5e6b776dc684ad3130866a1e7fe45b2a72da1
GOKUL29-06 <gokul2916s@gmail.com>
2024-11-14 17:45:03 +0530

    Front-End

---
+++ Employee-Management-System-Front-End/src/Routes/AdminRoute.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import PutRequestMain from '../Componants/Admin/PutRequest/PutRequestMain';
+import AdminNavBar from '../Componants/Admin/AdminNavBar';
+import AdminDashboard from '../Componants/Admin/AdminDashboard';
+import NewEmployee from '../Componants/Admin/NewEmployee';
+
+
+
+const AdminRoute = () => {
+    return (
+
+        <div className="d-flex ">
+            <AdminNavBar />
+            <div className="flex-grow-1 p-3" >
+                <Routes>
+                    <Route index element={<AdminDashboard />} />
+                    <Route path="/new-employee" element={<NewEmployee />} />
+
+                    <Route path="/dashboard" element={<AdminDashboard />} />
+                    {/* <Route path="/professional" element={<AddProfessionalDetails />} /> */}
+                    <Route path='/all-employees' element={<PutRequestMain />} />
+                    {/* <Route path="/projects" element={<Projects />} /> */}
+                    {/* <Route path="/finance" element={<DemoAddEmployee />} /> */}
+                    {/* <Route path="/logout" element={<Login />} /> */}
+                    {/* <Route path="/*" element={<NotFound />} /> 404 Route */}
+                </Routes>
+            </div>
+        </div>
+
+    );
+};
+
+export default AdminRoute;
